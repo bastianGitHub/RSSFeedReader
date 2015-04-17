031c542d083c363f2f816bc63ec44cf566919bf8
Bastian <bastian.lemke@hotmail.com>
2015-04-17 19:58:50 +0200

    1st Commit

---
+++ RSSFeedReader/RSSFeedReader.Shared/Model/RSSFeed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+
+namespace RSSFeedReader.Model
+{
+    public class RSSFeed
+    {
+        public string strFeedName { get; set; }
+        public Uri UriFeedIdentifier { get; set; }
+
+
+
+        public async Task<IList<SyndicationItem>> LoadNewsAsync()
+        {
+            SyndicationClient syndicationClient = new SyndicationClient();
+            var feeds = await syndicationClient.RetrieveFeedAsync(new Uri("http://golem.de.dynamic.feedsportal.com/pf/578068/http://rss.golem.de/rss.php?feed=RSS2.0"));
+
+            return feeds.Items;
+        }
+    }
+}
---
+++ RSSFeedReader/RSSFeedReader.Shared/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using RSSFeedReader.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Windows.Web.Syndication;
+
+namespace RSSFeedReader.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<SyndicationItem> News { get; set; }
+
+        public MainPageViewModel()
+        {
+            News = new ObservableCollection<SyndicationItem>();
+        }
+
+        public async void LoadNews()
+        {
+            RSSFeed rssFeedReader = new RSSFeed();
+
+            var news = await rssFeedReader.LoadNewsAsync();
+
+            foreach (SyndicationItem syndicationItem in news)
+            {
+                News.Add(syndicationItem);
+            }
+        }
+    }
+}
